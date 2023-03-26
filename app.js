8b049d95795835b116a6b4f2139f612fffb2b1c4
Issam-Addi <essam.h.addi@gmail.com>
2023-03-27 00:49:10 +0300

    first copy

--- app.js
+++ app.js
@@ -1,33 +1,59 @@
-let mainForm = document.getElementById("main-form")
-mainForm.addEventListener("submit", function(event) {
+
+let studentArray = [];
+if (localStorage.studentArray != null) {
+    studentArray = JSON.parse(localStorage.studentArray);
+    render();
+}
+
+//-------------add data-----------------//
+let mainForm = document.getElementById("main-form");
+mainForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    const userName = event.target.full_name.value;
-    const userAge = event.target.date_of_birth.value;
-    const userGender = event.target.gender.checked ? event.target.gender.value : "Female";
-    const userphone = event.target.phone_number.value;
-    const userGrade = event.target.grade.value;
-    let mainTableBody = document.getElementById("info");
-    let row = document.createElement("tr");
-
-        //////// fill tabel way A
-    // row.innerHTML = `<td>${userName}</td><td>${userAge}</td><td>${userGender}</td><td>${userphone}</td><td>${userGrade}</td>`;
-
-        //////// fill table way B
-    let cell1 = document.createElement("td");
-    cell1.textContent = userName;
-    row.appendChild(cell1);
-    let cell2 = document.createElement("td");
-    cell2.textContent = userAge;
-    row.appendChild(cell2);
-    let cell3 = document.createElement("td");
-    cell3.textContent = userGender;
-    row.appendChild(cell3);
-    let cell4 = document.createElement("td");
-    cell4.textContent = userphone;
-    row.appendChild(cell4);
-    let cell5 = document.createElement("td");
-    cell5.textContent = userGrade;
-    row.appendChild(cell5);
-    mainTableBody.appendChild(row);
+    let userName = event.target.full_name.value;
+    let userDate = event.target.date_of_birth.value;
+    let userGender = event.target.gender.value;
+    let userMajor = event.target.major.value;
+    let userImg = event.target.image.value;
+    let userNumber = event.target.phone_number.value;
     mainForm.reset();
-})
+    let studentInfo = new Person(userName, userDate, userGender, userMajor, userImg, userNumber);
+    studentArray.push(studentInfo);
+    console.log(studentArray);
+    localStorage.setItem('studentArray', JSON.stringify(studentArray));
+    mainForm.reset();
+});
+
+
+function Person(userName, userDate, userGender, userMajor, userImg, userNumber) {
+    this.userName = userName;
+    this.userDate = userDate;
+    this.userMajor = userMajor;
+    this.userImg = userImg;
+    this.userGender = userGender;
+    this.userNumber = userNumber;
+}
+
+function render(){
+    for (let i = 0; i < studentArray.length; i++) {
+        let mainIfoimage = document.getElementById("student-card");
+        let card = document.createElement("div");
+        card.classList.add("crad");
+        let stdImage = document.createElement("img");
+        stdImage.src= `${studentArray[i].userImg}`;
+        stdImage.alt= "student image";
+        card.appendChild(stdImage);
+        let stdMainInfo = document.createElement("div");
+        let stdName = document.createElement("p");
+        stdName.textContent = `Name: ${studentArray[i].userName}`;
+        stdMainInfo.appendChild(stdName);
+        let stdPhone = document.createElement("p");
+        stdPhone.textContent = `Phone number: ${studentArray[i].userNumber}`;
+        stdMainInfo.appendChild(stdPhone);
+        let stdMajor = document.createElement("p");
+        stdMajor.textContent = `Major: ${studentArray[i].userMajor}`;
+        stdMainInfo.appendChild(stdMajor);
+        card.appendChild(stdMainInfo);
+        mainIfoimage.appendChild(card);
+    }
+
+}
