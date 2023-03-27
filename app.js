82eb0094583372fda26c66c9f7c1837c2b7c44d9
Issam-Addi <essam.h.addi@gmail.com>
2023-03-27 09:53:38 +0300

    2nd copy

--- app.js
+++ app.js
@@ -1,59 +1,72 @@
+let stdArray = [];
 
-let studentArray = [];
-if (localStorage.studentArray != null) {
-    studentArray = JSON.parse(localStorage.studentArray);
-    render();
-}
 
-//-------------add data-----------------//
-let mainForm = document.getElementById("main-form");
-mainForm.addEventListener("submit", function (event) {
+let stdName;
+let stdDate;
+let stdGender;
+let stdMajor;
+let stdImage;
+let stdNumber;
+
+let studentInfo = document.getElementById("main-form");
+studentInfo.addEventListener("submit", function (event) {
     event.preventDefault();
-    let userName = event.target.full_name.value;
-    let userDate = event.target.date_of_birth.value;
-    let userGender = event.target.gender.value;
-    let userMajor = event.target.major.value;
-    let userImg = event.target.image.value;
-    let userNumber = event.target.phone_number.value;
-    mainForm.reset();
-    let studentInfo = new Person(userName, userDate, userGender, userMajor, userImg, userNumber);
-    studentArray.push(studentInfo);
-    console.log(studentArray);
-    localStorage.setItem('studentArray', JSON.stringify(studentArray));
-    mainForm.reset();
+    stdName = event.target.full_name.value;
+    stdDate = event.target.date_of_birth.value;
+    stdGender = event.target.gender.value;
+    stdMajor = event.target.major.value;
+    stdImage = event.target.image.value;
+    stdNumber = event.target.phone_number.value;
+    studentInfo.reset();
+    let student = new Person(stdName, stdDate, stdGender, stdNumber, stdMajor, stdImage);
+    stdArray.push(student);
+    localStorage.setItem('stdArray', JSON.stringify(stdArray));
+    render();
 });
 
 
-function Person(userName, userDate, userGender, userMajor, userImg, userNumber) {
-    this.userName = userName;
-    this.userDate = userDate;
-    this.userMajor = userMajor;
-    this.userImg = userImg;
-    this.userGender = userGender;
-    this.userNumber = userNumber;
+function Person(stdName, stdDate, stdGender, stdNumber, stdMajor, stdImage) {
+    this.stdName = stdName;
+    this.stdDate = stdDate;
+    this.stdGender = stdGender;
+    this.stdMajor = stdMajor;
+    this.stdImage = stdImage;
+    this.stdNumber = stdNumber;
 }
 
-function render(){
-    for (let i = 0; i < studentArray.length; i++) {
-        let mainIfoimage = document.getElementById("student-card");
-        let card = document.createElement("div");
-        card.classList.add("crad");
-        let stdImage = document.createElement("img");
-        stdImage.src= `${studentArray[i].userImg}`;
-        stdImage.alt= "student image";
-        card.appendChild(stdImage);
-        let stdMainInfo = document.createElement("div");
+
+
+//-------------show data-----------------//
+function render() {
+    for (let i = 0; i < stdArray.length; i++) {
+        let stdcard = document.getElementById("student-card");
+        let imageContent = document.createElement("div");
+        let image = document.createElement("img");
+        image.src = `${stdArray[i].stdImage}`;
+        imageContent.appendChild(image);
+
+        let stdInfoContent = document.createElement("div");
         let stdName = document.createElement("p");
-        stdName.textContent = `Name: ${studentArray[i].userName}`;
-        stdMainInfo.appendChild(stdName);
-        let stdPhone = document.createElement("p");
-        stdPhone.textContent = `Phone number: ${studentArray[i].userNumber}`;
-        stdMainInfo.appendChild(stdPhone);
-        let stdMajor = document.createElement("p");
-        stdMajor.textContent = `Major: ${studentArray[i].userMajor}`;
-        stdMainInfo.appendChild(stdMajor);
-        card.appendChild(stdMainInfo);
-        mainIfoimage.appendChild(card);
-    }
+        stdName.textContent = `${stdArray[i].stdName}`;
+        
 
+
+
+        let stdNumber = document.createElement("p");
+        stdNumber.textContent = `${stdArray[i].stdNumber}`;
+        
+
+
+
+
+        let stdMajor = document.createElement("p");
+        stdMajor.textContent = `${stdArray[i].stdMajor}`;
+        stdInfoContent.appendChild(stdName);
+        stdInfoContent.appendChild(stdNumber);
+        stdInfoContent.appendChild(stdMajor);
+        stdcard.appendChild(imageContent);
+        stdcard.appendChild(stdInfoContent);
+    }
 }
+
+
